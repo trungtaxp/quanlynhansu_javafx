32bcc4505f4d3d564184a106e44ae992430b8683
kingcats10012001 <phamtrungta2001@gmail.com>
2021-07-22 02:43:50 +0700

    v1

---
+++ src/quanlynhansu/main/main.java
@@ -0,0 +1,22 @@
+package quanlynhansu.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../giaodien/main.fxml"));
+        primaryStage.setTitle("A34684 - A36000 - A37181 - Đề 5");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
